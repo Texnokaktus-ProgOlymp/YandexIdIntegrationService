07d5912f25ccc8be57a16600a45aca6b49f1b265
Artem Krylov <kav128@outlook.com>
2025-02-03 13:30:18 +0300

    add all required libs

--- Texnokaktus.ProgOlymp.YandexIdIntegrationService/Program.cs
+++ Texnokaktus.ProgOlymp.YandexIdIntegrationService/Program.cs
@@ -1,4 +1,9 @@
+using System.Reflection;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using StackExchange.Redis;
+using Texnokaktus.ProgOlymp.OpenTelemetry;
 using Texnokaktus.ProgOlymp.YandexIdIntegrationService.DataAccess;
 using Texnokaktus.ProgOlymp.YandexIdIntegrationService.Logic;
 using Texnokaktus.ProgOlymp.YandexIdIntegrationService.Services.Grpc;
@@ -11,9 +16,35 @@ builder.Services
        .AddYandexClient()
        .AddLogic();
 
+var connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync(builder.Configuration.GetConnectionString("DefaultRedis")!);
+builder.Services.AddSingleton<IConnectionMultiplexer>(connectionMultiplexer);
+builder.Services.AddStackExchangeRedisCache(options => options.ConnectionMultiplexerFactory = () => Task.FromResult<IConnectionMultiplexer>(connectionMultiplexer));
+builder.Services.AddMemoryCache();
+
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
+builder.Services
+       .AddGrpcHealthChecks()
+       .AddDatabaseHealthChecks();
+
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+
+builder.Services.AddTexnokaktusOpenTelemetry(builder.Configuration, "YandexContestIntegrationService", null, null);
+
+builder.Services
+       .AddDataProtection(options => options.ApplicationDiscriminator = Assembly.GetEntryAssembly()?.GetName().Name)
+       .PersistKeysToStackExchangeRedis(connectionMultiplexer);
 
 var app = builder.Build();
+
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
+
+app.MapGrpcHealthChecksService();
 
 app.MapGrpcService<UserServiceImpl>();
 
